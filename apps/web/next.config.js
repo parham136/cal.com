09dbd1713668288601da8144079f1a7d2c8d98ae
Parham Khosrov <p.khosro@gmail.com>
2025-04-16 16:11:51 +0100

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,19 +1,11 @@
 require("dotenv").config({ path: "../../../.env" });
 
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const os = require("os");
-const englishTranslation = require("./public/static/locales/en/common.json");
 const { withAxiom } = require("next-axiom");
 const { withSentryConfig } = require("@sentry/nextjs");
 const { version } = require("./package.json");
-const { i18n: { locales } } = require("./next-i18next.config");
 
-const nextJsOrgRewriteConfig = {};
-const orgUserRoutePath = {};
-const orgUserTypeRoutePath = {};
-const orgUserTypeEmbedRoutePath = {};
-
-// ENV guards
+// Environment checks
 if (process.env.NODE_ENV !== "production" && !process.env.NEXTAUTH_SECRET) {
   throw new Error("Please set NEXTAUTH_SECRET");
 }
@@ -27,24 +19,14 @@ if (!process.env.NEXT_PUBLIC_API_V2_URL) {
   console.error("Please set NEXT_PUBLIC_API_V2_URL");
 }
 
-const isOrganizationsEnabled =
-  process.env.ORGANIZATIONS_ENABLED === "1" ||
-  process.env.ORGANIZATIONS_ENABLED === "true";
-
 const config = {
-  i18n: { locales },
   reactStrictMode: true,
-  swcMinify: true,
   productionBrowserSourceMaps: true,
   env: {
     NEXT_PUBLIC_CALCOM_VERSION: version,
   },
   rewrites: async () => ({
     beforeFiles: [
-      {
-        source: `/:path*`,
-        destination: "/:path*",
-      },
       {
         source: "/forms/:formQuery*",
         destination: "/apps/routing-forms/routing-link/:formQuery*",
