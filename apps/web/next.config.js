80909e61ff60e8b5e11b78aa204ee05ee27037fa
Parham Khosrov <p.khosro@gmail.com>
2025-04-16 16:05:07 +0100

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,31 +1,28 @@
-require("dotenv").config({ path: "./.env" });
+require("dotenv").config({ path: "../../../.env" });
+
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const os = require("os");
 const englishTranslation = require("./public/static/locales/en/common.json");
 const { withAxiom } = require("next-axiom");
 const { withSentryConfig } = require("@sentry/nextjs");
 const { version } = require("./package.json");
-const { i18n } = require("./next-i18next.config");
+const { i18n: { locales } } = require("./next-i18next.config");
 
-const {
-  nextJsOrgRewriteConfig,
-  orgUserRoutePath,
-  orgUserTypeRoutePath,
-  orgUserTypeEmbedRoutePath,
-} = require("./.pagesAndRewritePaths");
+const nextJsOrgRewriteConfig = {};
+const orgUserRoutePath = {};
+const orgUserTypeRoutePath = {};
+const orgUserTypeEmbedRoutePath = {};
 
+// ENV guards
 if (process.env.NODE_ENV !== "production" && !process.env.NEXTAUTH_SECRET) {
   throw new Error("Please set NEXTAUTH_SECRET");
 }
-
 if (process.env.NODE_ENV !== "production" && !process.env.CALENDSO_ENCRYPTION_KEY) {
   throw new Error("Please set CALENDSO_ENCRYPTION_KEY");
 }
-
 if (process.env.NODE_ENV !== "production" && !process.env.NEXTAUTH_URL) {
   throw new Error("Please set NEXTAUTH_URL");
 }
-
 if (!process.env.NEXT_PUBLIC_API_V2_URL) {
   console.error("Please set NEXT_PUBLIC_API_V2_URL");
 }
@@ -35,117 +32,38 @@ const isOrganizationsEnabled =
   process.env.ORGANIZATIONS_ENABLED === "true";
 
 const config = {
-  i18n,
+  i18n: { locales },
+  reactStrictMode: true,
+  swcMinify: true,
+  productionBrowserSourceMaps: true,
   env: {
-    CAL_VERSION: version,
+    NEXT_PUBLIC_CALCOM_VERSION: version,
   },
-  webpack(config) {
-    config.plugins.push(
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: "./.well-known",
-            to: "../.well-known",
-          },
-        ],
-      })
-    );
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      os: require.resolve("os-browserify/browser"),
-    };
-
-    return config;
-  },
-  async rewrites() {
-    const { orgSlug } = nextJsOrgRewriteConfig;
-
-    const beforeFiles = [
+  rewrites: async () => ({
+    beforeFiles: [
       {
-        source: `/:locale/:path*`,
+        source: `/:path*`,
         destination: "/:path*",
       },
       {
         source: "/forms/:formQuery*",
         destination: "/apps/routing-forms/routing-link/:formQuery*",
       },
-      {
-        source: "/routing",
-        destination: "/routing/forms",
-      },
-      {
-        source: "/routing/:path*",
-        destination: "/apps/routing-forms/:path*",
-      },
-      {
-        source: "/success/:path*",
-        has: [{ type: "query", key: "uid", value: "(?<uid>.*)" }],
-        destination: "/booking/:uid/:path*",
-      },
-      {
-        source: "/cancel/:path*",
-        destination: "/booking/:path*",
-      },
-      {
-        source: "/embed.js",
-        destination: "/embed/embed.js",
-      },
-      {
-        source: "/login",
-        destination: "/auth/login",
-      },
-      ...(isOrganizationsEnabled
-        ? [
-            {
-              source: orgUserRoutePath.source,
-              destination: `/org/${orgSlug}/:user`,
-            },
-            {
-              source: orgUserTypeRoutePath.source,
-              destination: `/org/${orgSlug}/:user/:type`,
-            },
-            {
-              source: orgUserTypeEmbedRoutePath.source,
-              destination: `/org/${orgSlug}/:user/:type/embed`,
-            },
-          ]
-        : []),
-    ];
-
-    const afterFiles = [];
-
-    if (
-      process.env.NEXT_PUBLIC_API_V2_URL &&
-      (process.env.NEXT_PUBLIC_API_V2_URL.startsWith("/") ||
-        process.env.NEXT_PUBLIC_API_V2_URL.startsWith("http"))
-    ) {
-      afterFiles.push({
-        source: "/api/v2/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_V2_URL}/:path*`,
-      });
-    }
-
-    if (
-      process.env.NEXT_PUBLIC_WEBAPP_URL &&
-      (process.env.NEXT_PUBLIC_WEBAPP_URL.startsWith("/") ||
-        process.env.NEXT_PUBLIC_WEBAPP_URL.startsWith("http"))
-    ) {
-      afterFiles.push({
-        source: "/icons/sprite.svg",
-        destination: `${process.env.NEXT_PUBLIC_WEBAPP_URL}/icons/sprite.svg`,
-      });
-    }
-
-    return {
-      beforeFiles,
-      afterFiles,
-    };
+    ],
+  }),
+  webpack: (config) => {
+    config.plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "./public/static/locales",
+            to: "public/static/locales",
+          },
+        ],
+      })
+    );
+    return config;
   },
 };
 
-module.exports = withAxiom(withSentryConfig(config));
+module.exports = withSentryConfig(withAxiom(config));
